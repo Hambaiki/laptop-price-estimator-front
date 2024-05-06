ca6dc27e6680f544c7acf7c4640664b74e21dc3e
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2024-05-06 14:18:28 +0700

    First major updates
    - More complete page content
    - Polish stylings
    - Implement updated API and associated operations
    - UX improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,40 @@ import LaptopSelector from "@/components/LaptopSelector";
 
 async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-full">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 lg:p-24">
+      <header className="w-full">
         <h1 className="text-4xl font-bold">Laptop Price Estimator</h1>
         <p className="text-gray-500 mt-2">
-          Powered by a machine learning model
+          Estimate your laptop budget based on your desired specifications.
         </p>
-      </div>
+      </header>
 
-      <div className="mt-8 w-full">
+      <article className="mt-8 w-full">
+        <h2 className="text-xl font-semibold">How does it work?</h2>
+        <p className="mt-2 text-gray-700">
+          Simply input the desired specification and the input will be
+          processed. The results will include predictions generated the trained
+          machine learning model. machine learning model
+        </p>
+        <p className="mt-2 text-gray-400 text-sm">
+          <strong>Note: </strong>The model is trained from a dataset with INR
+          currency. The estimated price is converted from INR to USD at the
+          current rate of 0.012 as of{" "}
+          {new Date("2024/5/5").toLocaleDateString()}
+        </p>
+      </article>
+
+      <section className="mt-8 w-full">
         <LaptopSelector />
-      </div>
+      </section>
 
-      {/* <div className="mt-8 w-full">
-        <h2 className="text-xl font-semibold">Introduction</h2>
-        <p className="mt-2">something</p>
-        <p className="mt-2">something</p>
-      </div> */}
+      <footer className="text-gray-500 text-sm mt-8">
+        <p>
+          <span className="font-semibold">LPA Model Implementation</span> |
+          Developed by
+          <span className="font-semibold"> PWK.</span>
+        </p>
+      </footer>
     </main>
   );
 }
--- src/components/LaptopSelector.tsx
+++ src/components/LaptopSelector.tsx
@@ -1,21 +1,30 @@
 "use client";
 
-import { useState } from "react";
-import { laptopOptions } from "../constants/laptopOptions";
+import { useEffect, useState } from "react";
+import { laptopOptions, optionKeyMapping } from "../constants/laptopOptions";
 import apiInstance from "@/utils/axiosInstance";
+import { FaSpinner } from "react-icons/fa";
 
-interface result {
-  "results ": {
-    linear_regression: number[];
-    random_forest: number[];
-    gradient_boosting: number[];
-  };
+interface ResultResponseI {
+  linear_regression: number[];
+  random_forest: number[];
+  gradient_boosting: number[];
 }
+
+interface ResultI {
+  linearRegression: number;
+  randomForest: number;
+  gradientBoosting: number;
+}
+
 function LaptopSelector() {
-  const [result, setResult] = useState<any>();
-  const [selectedOptions, setSelectedOptions] = useState<
-    Record<string, string | number>
-  >({
+  const [result, setResult] = useState<ResultI>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isOptionsValid, setIsOptionsValid] = useState(true);
+
+  const [selectedOptions, setSelectedOptions] = useState<{
+    [key: string]: number | string;
+  }>({
     laptop_brand: "",
     ram: 0,
     ssd: 0,
@@ -30,41 +39,60 @@ function LaptopSelector() {
   });
 
   async function predictResult() {
-    const features: string[] = [];
-    Object.entries(selectedOptions).forEach(([key, value]) => {
-      if (key in laptopOptions) {
-        const option = laptopOptions[key];
-        features.push(
-          Object.keys(option).find((key) => option[key] === value) ?? "0"
-        );
-      } else {
-        features.push(value.toString());
-      }
-    });
-
-    console.log(features);
+    setIsLoading(true);
 
     const req = {
-      input: features.map((value) => parseInt(value)),
+      ram: selectedOptions["ram"],
+      ssd: selectedOptions["ssd"],
+      hdd: selectedOptions["hdd"],
+      screen_size: optionKeyMapping.size[selectedOptions["size"]] ?? 0,
+      screen_resolution:
+        optionKeyMapping.resolution[selectedOptions["resolution"]] ?? 0,
+      no_of_cores: selectedOptions["no_of_cores"],
+      no_of_threads: selectedOptions["no_of_threads"],
+      brand: optionKeyMapping.laptopBrand[selectedOptions["laptop_brand"]] ?? 0,
+      cpu_brand: optionKeyMapping.cpuBrand[selectedOptions["cpu_brand"]] ?? 0,
+      gpu_brand: optionKeyMapping.gpuBrand[selectedOptions["gpu_brand"]] ?? 0,
+      os: optionKeyMapping.os[selectedOptions["os"]] ?? 0,
     };
-    const response = await apiInstance.post("/predict", req);
 
-    const rf = response.data["results "]["random_forest"]
-    console.log(rf);
+    try {
+      const response = await apiInstance.post<ResultResponseI>("/predict", req);
 
-    setResult(Math.round(rf))
+      setResult({
+        linearRegression: Math.round(
+          response.data.linear_regression[0] * 0.012
+        ),
+        randomForest: Math.round(response.data.random_forest[0] * 0.012),
+        gradientBoosting: Math.round(
+          response.data.gradient_boosting[0] * 0.012
+        ),
+      });
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
-  const handleOptionChange = (category: string, value: string) => {
+  function handleOptionChange(category: string, value: string) {
     setSelectedOptions({ ...selectedOptions, [category]: value });
-  };
+  }
 
-  const handleNumericChange = (category: string, value: string) => {
+  function handleNumericChange(category: string, value: string) {
     const parsedString = parseInt(value);
     if (parsedString >= 0) {
       setSelectedOptions({ ...selectedOptions, [category]: value });
     }
-  };
+  }
+
+  // useEffect(() => {
+  //   setIsOptionsValid(
+  //     Object.values(selectedOptions).every(
+  //       (value) => typeof value !== 'number' && value
+  //     )
+  //   );
+  // }, [selectedOptions]);
 
   const numericOptions = [
     {
@@ -89,11 +117,21 @@ function LaptopSelector() {
     },
   ];
   return (
-    <div className="w-full border rounded-lg p-6">
-      <p>Estimated Price:</p>
-      <div className="flex items-center justify-center border p-4 mt-2 rounded h-32">
+    <div className="w-full border rounded-lg p-6 bg-gray-50">
+      <p className="font-medium">Estimated Price:</p>
+      <div className="relative flex items-center justify-center border p-4 mt-2 rounded h-32 bg-gray-100">
+        <div
+          className={`flex absolute inset-0 backdrop-blur transition-all ${
+            isLoading ? "opacity-100 " : "opacity-0 pointer-events-none"
+          }`}
+        >
+          <FaSpinner
+            className={`m-auto w-8 h-8 ${isLoading && "animate-spin"}`}
+          />
+        </div>
+
         {result ? (
-          <p className="text-5xl italic">{result} INR</p>
+          <p className="text-5xl italic">{result.randomForest} USD</p>
         ) : (
           <p className="text-gray-500 text-xl">Awaiting Input...</p>
         )}
@@ -102,23 +140,21 @@ function LaptopSelector() {
         {Object.entries(laptopOptions).map(([key, value], index) => (
           <div key={index} className="flex items-center justify-between">
             <label className="w-32 shrink-0" htmlFor={key}>
-              {key}:
+              {value.title}:
             </label>
             <select
               defaultValue={undefined}
-              className="truncate px-4 py-2 w-full rounded bg-gray-900 border border-gray-700"
+              className="truncate px-4 py-2 w-full rounded bg-gray-200 border border-gray-300"
               id="laptopBrand"
-              value={selectedOptions[key]}
-              onChange={(e) => handleOptionChange(key, e.target.value)}
+              value={selectedOptions[value.name]}
+              onChange={(e) => handleOptionChange(value.name, e.target.value)}
             >
-              <option value={undefined}>Select option</option>
-              {Object.entries(value).map(
-                ([optionIndex, optionValue], index) => (
-                  <option key={index} value={optionValue}>
-                    {optionValue}
-                  </option>
-                )
-              )}
+              <option value="">Select option</option>
+              {value.options.map((option, index) => (
+                <option key={index} value={option.value}>
+                  {option.value}
+                </option>
+              ))}
             </select>
           </div>
         ))}
@@ -128,7 +164,7 @@ function LaptopSelector() {
               {option.title}
             </label>
             <input
-              className="truncate px-4 py-2 w-full rounded bg-gray-900 border border-gray-700"
+              className="truncate px-4 py-2 w-full rounded bg-gray-200 border border-gray-300"
               type="number"
               id="ram"
               value={selectedOptions[option.name]}
@@ -141,8 +177,22 @@ function LaptopSelector() {
         <button
           type="button"
           onClick={predictResult}
-          className="w-full h-12 mt-8 flex items-center justify-center border rounded-lg hover:bg-gray-900 transition-colors"
+          disabled={!isOptionsValid}
+          className={`group relative w-full h-12 mt-8 flex items-center justify-center border rounded 
+                    ${
+                      isOptionsValid
+                        ? "border-gray-400 hover:bg-gray-200 transition-colors"
+                        : "border-gray-200 text-gray-300"
+                    }`}
         >
+          <div
+            className={`opacity-0 absolute -top-14 left-1/2 transform -translate-x-1/2 pointer-events-none transition-all bg-amber-500 text-white px-4 py-2 rounded
+                      ${
+                        isOptionsValid ? "hidden" : "group-hover:opacity-100 "
+                      }`}
+          >
+            Please select all options
+          </div>
           Submit
         </button>
       </div>
